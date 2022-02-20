e397f644f675570e70556eb8283f2b3aedd4b129
PauloRedkva <paulo.redkva@alunos.fael.edu.br>
2022-02-20 11:15:18 -0300

    Script - Calculadora Python com apenas 3 linhas de Código.

---
+++ calculadoraPython.py
@@ -0,0 +1,10 @@
+"""
+Calculadora Python com apenas 3 linhas de Código!
+
+==> Esta calculadora não possue interface gráfica.
+"""
+math_expression = input("Digite uma expressão matemática: ")
+
+result = eval(math_expression)
+
+print("O resultado é: ", result)
